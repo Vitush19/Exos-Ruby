9389e9ef3c426d37f7355ea0b72ed7db944032f4
Vitush19 <sutharsanvithusan@gmail.com>
2020-09-29 23:21:00 +0100

    exercices completed

--- exo_21.rb
+++ exo_21.rb
@@ -4,13 +4,13 @@ number = gets.to_i
 hashtag = "#"
 white_space = " "
 number_white_space = number - 1
-number_white_space.times do |j|
-  white_space += " "
-end
 puts "Voici la pyramide :"
 number.times do |i|
-  puts "#{white_space}"
+  for j in number_white_space.downto(i)
+    white_space += " "
+  end
+  print "#{white_space}"
   puts "#{hashtag}"
   hashtag += "#"
-  white_space = white_space - " "
+  white_space = " "
 end
